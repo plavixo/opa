8091c7762ad67be32267b20a8bfa3941d1021bf6
Teresa Binks <teresa@teresabinks.co.uk>
2020-01-05 15:29:36 +0000

    Changed tests to RoomPriority Tests

--- AppointmentGeneratorTests/RoomPriorityTests.cs
+++ AppointmentGeneratorTests/RoomPriorityTests.cs
@@ -2,13 +2,40 @@
 using FluentAssertions;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
 namespace AppointmentGeneratorTests
 {
-	public class GivenPreferredTimeNotAvailableInPreferredRoom
+	public class RoomPriorityTests
 	{
+		[Fact]
+		public void ShouldPreferRoomAlpha()
+		{
+			//arrange
+			Mock<IRoomAvailabilityAdaptor> mockAvailabilityAdaptor = new Mock<IRoomAvailabilityAdaptor>();
+			mockAvailabilityAdaptor
+				.Setup(a => a.RoomIsAvailbleAtTime(It.IsAny<Room>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+				.Returns(true);
+			
+
+			IRoomAvailabilityAdaptor roomAvailabilityAdaptor = mockAvailabilityAdaptor.Object;
+
+			DateTime start = new DateTime(2020, 1, 1);
+			DateTime end = new DateTime(2020, 1, 9);
+
+			IList<Room> locations = new List<Room>() { Room.Alpha, Room.Bravo };
+			IAppointmentBuildable appointmentWithLocations = new AppointmentWithLocations(locations,null);
+			IAppointmentBuildable testAppointment = new AppointmentWithTimes(start, end, appointmentWithLocations);
+
+			//act
+			var flattenedAppointment = new Prioritiser(roomAvailabilityAdaptor).Flatten(testAppointment);
+
+			//assert
+			flattenedAppointment.Location.Should().Be(Room.Alpha);
+		}
+
 		[Fact]
 		public void ShouldFallBackToSecondaryRoom() {
 			//arrange
@@ -31,10 +58,6 @@ namespace AppointmentGeneratorTests
 
 			//assert
 			flattenedAppointment.Location.Should().Be(Room.Bravo);
-		}
-
-		[Fact(Skip = "Placeholding for completeness")]
-		public void ShouldFallBackToSecondaryTime() { 
 		}
 	}	
 }
