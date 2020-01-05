d290811cb9a2f31586b99a674f6c051e20f85548
Teresa Binks <teresa@teresabinks.co.uk>
2020-01-05 21:09:08 +0000

    Made test set up a little more readable

--- AppointmentGeneratorTests/AppointmentSetBigScopeTests.cs
+++ AppointmentGeneratorTests/AppointmentSetBigScopeTests.cs
@@ -48,38 +48,51 @@ namespace AppointmentGeneratorTests
 
         public AppointmentSetBigScopeTestsFixture()
         {
+
+            //Arrange
             var demoToProcess = new AppointmentGenerator(new Demo()).GetAppointmentsThatFallWithin(StartDate, EndDate).Single();
             var mipToProcess = new AppointmentGenerator(new MIP()).GetAppointmentsThatFallWithin(StartDate, EndDate).Single();
             var planningToProcess = new AppointmentGenerator(new Planning()).GetAppointmentsThatFallWithin(StartDate, EndDate).Single();
-            
-            IList<IAppointmentBuildable> potentialAppointments = new List<IAppointmentBuildable>() {
+
+            IList<IAppointmentBuildable> setOfAppointments = new List<IAppointmentBuildable>() {
                 demoToProcess, mipToProcess, planningToProcess
             };
+            
+            IList<Room> desiredRoomsForSet = new List<Room> { Room.Delta, Room.Echo };
 
-            Mock<IRoomAvailabilityAdaptor> mockRoomAvailabilityAdaptor = new Mock<IRoomAvailabilityAdaptor>();
-            mockRoomAvailabilityAdaptor
-                .Setup(a => a.RoomIsAvailbleAtTime(It.IsAny<Room>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
-                .Returns((Room room, DateTime start, DateTime end)=> thing(room, start, end));
+            IRoomAvailabilityAdaptor roomAvailabilityAdaptor = GetAvailablilityAdaptorThatWillShowRoomBusyAtAppointmentTime(mipToProcess);
 
-            bool thing(Room room, DateTime start, DateTime end)
-            {
-                if(room.Equals(Room.Delta) && start.Equals(mipToProcess.TimeBlock.StartTime) && end.Equals(mipToProcess.TimeBlock.EndTime)){
-                    return false;
-                }else {
-                    return true;
-                }
-            }
 
-            IRoomAvailabilityAdaptor roomAvailabilityAdaptor = mockRoomAvailabilityAdaptor.Object;
+            // Act
+            IEnumerable<IAppointment> appointments = new Prioritiser(roomAvailabilityAdaptor).FlattenSet_SameRoom_PrioritiseTime(setOfAppointments, desiredRoomsForSet);
 
-            IList<Room> desiredRooms = new List<Room> { Room.Delta, Room.Echo};
-            IEnumerable<IAppointment> appointments = new Prioritiser(roomAvailabilityAdaptor).FlattenSet_SameRoom_PrioritiseTime(potentialAppointments, desiredRooms);
-
+            //Unpack for tests
             Demo = appointments.Where(a => a.Subject.Equals(TestTypes.DemoTitle)).Single();
             MIP = appointments.Where(a => a.Subject.Equals(TestTypes.MIPTitle)).Single();
             Planning = appointments.Where(a => a.Subject.Equals(TestTypes.PlanningTitle)).Single();
         }
 
-       
+        private static IRoomAvailabilityAdaptor GetAvailablilityAdaptorThatWillShowRoomBusyAtAppointmentTime(IAppointmentBuildable appointment)
+        {
+            Mock<IRoomAvailabilityAdaptor> mockRoomAvailabilityAdaptor = new Mock<IRoomAvailabilityAdaptor>();
+            mockRoomAvailabilityAdaptor
+                .Setup(a => a.RoomIsAvailbleAtTime(It.IsAny<Room>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .Returns((Room room, DateTime start, DateTime end) => thing(room, start, end));
+
+            bool thing(Room room, DateTime start, DateTime end)
+            {
+                if (room.Equals(Room.Delta) && start.Equals(appointment.TimeBlock.StartTime) && end.Equals(appointment.TimeBlock.EndTime))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return mockRoomAvailabilityAdaptor.Object;
+        }
+
     }
 }
